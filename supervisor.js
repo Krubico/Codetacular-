faa88c795d39ab01d444ef15d5343d029bb01186
Gigiwong890 <107204089+Gigiwong890@users.noreply.github.com>
2022-06-11 18:51:30 +0800

    Add files via upload

---
+++ supervisor.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+function SupervisorAdd() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SupervisorHistory() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="add" component={SupervisorAdd} />
+        <Tab.Screen name="history" component={SupervisorHistory} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
